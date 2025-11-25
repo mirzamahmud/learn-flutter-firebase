781086effd03083eb268bcb14bdf7049a8729c5d
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-11-25 16:07:49 +0600

    create app colors

---
+++ lib/core/ui/color/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+  static const Color grey = Colors.grey;
+  static const Color yellow = Colors.amber;
+
+  static const Color success = Colors.green;
+  static const Color error = Colors.red;
+  static const Color warning = Colors.orange;
+}
