fa4920ad59455cf37642666c06894a81ccee4b5d
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-11-25 16:08:56 +0600

    create example view

---
+++ lib/features/example/example_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:learn_flutter_firebase/features/example/view/example_view.dart';
+
+class ExamplePage extends StatelessWidget {
+  const ExamplePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ExampleView();
+  }
+}
---
+++ lib/features/example/view/example_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:learn_flutter_firebase/core/global/extensions/context_extension.dart';
+
+class ExampleView extends StatefulWidget {
+  const ExampleView({super.key});
+
+  @override
+  State<ExampleView> createState() => _ExampleViewState();
+}
+
+class _ExampleViewState extends State<ExampleView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Example', style: context.theme.appBarTheme.titleTextStyle),
+      ),
+      body: SafeArea(
+        top: false,
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text('Example View')],
+          ),
+        ),
+      ),
+    );
+  }
+}
