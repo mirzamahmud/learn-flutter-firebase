15367e17f49aeba679f84854f2c27c4e223c9c1b
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-11-25 16:08:45 +0600

    create splash view

---
+++ lib/features/intro/splash/splash_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:learn_flutter_firebase/features/intro/splash/view/splash_view.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SplashView();
+  }
+}
---
+++ lib/features/intro/splash/view/splash_view.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:learn_flutter_firebase/core/global/constants/app/app_assets.dart';
+import 'package:learn_flutter_firebase/core/global/extensions/context_extension.dart';
+import 'package:learn_flutter_firebase/core/routes/app_routes.dart';
+import 'package:learn_flutter_firebase/services/navigation_service.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Timer(
+    //   const Duration(seconds: 4),
+    //   () => NavigationService.pushAndRemove(AppRoutes.example),
+    // );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: false,
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 12,
+            children: [
+              Image.asset(AppAssets.appLogo, height: 96, width: 96),
+              Text(
+                'Learn Flutter Firebase',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: context.theme.colorScheme.onSurface,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
