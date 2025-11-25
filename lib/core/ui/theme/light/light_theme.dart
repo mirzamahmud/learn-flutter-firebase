6040a025414392f5510020d686b23232bae96caf
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-11-25 16:08:29 +0600

    create light theme

---
+++ lib/core/ui/theme/light/light_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:learn_flutter_firebase/core/ui/color/app_colors.dart';
+import 'package:learn_flutter_firebase/core/ui/theme/light/appbar_theme/light_appbar_theme.dart';
+import 'package:learn_flutter_firebase/core/ui/theme/light/color_scheme/light_color_scheme.dart';
+
+class LightTheme {
+  static ThemeData light = ThemeData(
+    scaffoldBackgroundColor: AppColors.white,
+    brightness: LightColorScheme.colorScheme.brightness,
+    colorScheme: LightColorScheme.colorScheme,
+    primaryColor: LightColorScheme.colorScheme.primary,
+    shadowColor: LightColorScheme.colorScheme.shadow,
+    unselectedWidgetColor: LightColorScheme.colorScheme.outline,
+    appBarTheme: LightAppbarTheme.appBarTheme,
+  );
+}
