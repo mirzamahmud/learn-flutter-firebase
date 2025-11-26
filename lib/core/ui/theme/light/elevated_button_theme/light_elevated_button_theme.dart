f00737fe829120eb0a545c22fa190479e9c551a5
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-11-26 16:14:49 +0600

    modify elevated button light theme

--- lib/core/ui/theme/light/elevated_button_theme/light_elevated_button_theme.dart
+++ lib/core/ui/theme/light/elevated_button_theme/light_elevated_button_theme.dart
@@ -5,6 +5,27 @@ class LightElevatedButtonTheme {
   static final ElevatedButtonThemeData _elevatedButtonThemeData =
       ElevatedButtonThemeData(
         style: ButtonStyle(
+          shape: WidgetStatePropertyAll(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadiusGeometry.circular(8),
+              side: BorderSide.none,
+            ),
+          ),
+          iconColor: WidgetStatePropertyAll(
+            LightColorScheme.colorScheme.onPrimary,
+          ),
+          iconAlignment: IconAlignment.start,
+          iconSize: WidgetStatePropertyAll(20),
+          padding: WidgetStatePropertyAll(
+            const EdgeInsetsDirectional.symmetric(vertical: 12, horizontal: 24),
+          ),
+          backgroundColor: WidgetStatePropertyAll(
+            LightColorScheme.colorScheme.primary,
+          ),
+          foregroundColor: WidgetStatePropertyAll(
+            LightColorScheme.colorScheme.onPrimary,
+          ),
+          elevation: WidgetStatePropertyAll(0),
           textStyle: WidgetStatePropertyAll(
             TextStyle(
               color: LightColorScheme.colorScheme.onPrimary,
