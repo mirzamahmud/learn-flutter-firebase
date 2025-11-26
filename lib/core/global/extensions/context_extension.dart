06f0e000fe8c5ea655c6197f142eafe59ccee4e4
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-11-26 16:15:13 +0600

    create context extension for screen width

--- lib/core/global/extensions/context_extension.dart
+++ lib/core/global/extensions/context_extension.dart
@@ -2,4 +2,7 @@ import 'package:flutter/material.dart';
 
 extension ContextExtension on BuildContext {
   ThemeData get theme => Theme.of(this);
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
+
+  double get screenWidth => mediaQuery.size.width;
 }
