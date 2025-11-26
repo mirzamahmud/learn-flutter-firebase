56c5666f421d2080c0ff1e77bb976b110295796e
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-11-26 16:15:36 +0600

    create reusable widget for primary button

--- lib/core/widgets/buttons/primary_button.dart
+++ lib/core/widgets/buttons/primary_button.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter_firebase/core/global/extensions/context_extension.dart';
 
 class PrimaryButton extends StatelessWidget {
-  const PrimaryButton({super.key});
+  const PrimaryButton({
+    super.key,
+    required this.buttonLabel,
+    this.onPressed,
+    this.height = 48,
+  });
+
+  final String buttonLabel;
+  final void Function()? onPressed;
+  final double height;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: context.theme.elevatedButtonTheme.style?.copyWith(
+        fixedSize: WidgetStatePropertyAll(Size(context.screenWidth, height)),
+      ),
+      child: Text(buttonLabel),
+    );
   }
 }
